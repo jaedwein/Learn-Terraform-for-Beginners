23d63c0c66faead62d85aa36215d3f223274b55e
Your Name <you@example.com>
2023-05-18 15:04:20 -0300

    Se agrega sunet pública

--- main.tf
+++ main.tf
@@ -10,3 +10,14 @@ resource "aws_vpc" "mtc_vpc" {
     Name = "Dev"
   }
 }
+
+resource "aws_subnet" "mtc_public_subnet" {
+    vpc_id = aws_vpc.mtc_vpc.id
+    cidr_block = "10.128.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1c"
+
+    tags = {
+      Name = "dev-public-subnet"
+    }
+}
