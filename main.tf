80af6f79cd87c756d8de4cd4316b61ffa834ae1d
Your Name <you@example.com>
2023-05-19 01:17:57 -0300

    Se agrega IGW, route table y route

--- main.tf
+++ main.tf
@@ -12,12 +12,33 @@ resource "aws_vpc" "mtc_vpc" {
 }
 
 resource "aws_subnet" "mtc_public_subnet" {
-    vpc_id = aws_vpc.mtc_vpc.id
-    cidr_block = "10.128.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1c"
+  vpc_id                  = aws_vpc.mtc_vpc.id
+  cidr_block              = "10.128.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1c"
 
-    tags = {
-      Name = "dev-public-subnet"
-    }
+  tags = {
+    Name = "dev-public-subnet"
+  }
+}
+
+resource "aws_internet_gateway" "mtc_internet_gateway" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "dev-igw"
+  }
+}
+
+resource "aws_route_table" "mtc_public_rt" {
+  vpc_id = aws_vpc.mtc_vpc.id
+  tags = {
+    Name = "dev_public_rt"
+  }
+}
+
+
+resource "aws_route" "defaul_route" {
+  route_table_id         = aws_route_table.mtc_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.mtc_internet_gateway.id
 }
